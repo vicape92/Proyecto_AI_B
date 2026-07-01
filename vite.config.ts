f6552e274898a0a650ece68e3bfef925573bf36c
vicape92 <vicape92@gmail.com>
2026-07-01 20:35:32 +0200

    Update Vite config for React and project base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  server: {
-    host: "127.0.0.1",
-  },
-});
+  plugins: [react()],
+  base: '/Proyecto_AI_B/', 
+})
